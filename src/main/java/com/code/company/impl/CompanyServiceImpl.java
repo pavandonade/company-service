7dd8567b28676b87cdb892621a4cd49103fafeaa
Pavan Donade <pavandonade.dev@gmail.com>
2025-10-30 19:53:51 +0530

    company-service

---
+++ src/main/java/com/code/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,58 @@
+package com.code.company.impl;
+
+import com.code.company.Company;
+import com.code.company.CompanyRepository;
+import com.code.company.CompanyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company, Long id) {
+        Optional<Company> optionalCompany = companyRepository.findById(id);
+        if (optionalCompany.isPresent()) {
+            Company comp= optionalCompany.get();
+            comp.setDescription(company.getDescription());
+            comp.setName(company.getName());
+            companyRepository.save(comp);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompanyById(Long id) {
+        if (companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+
+}
